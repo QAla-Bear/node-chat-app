906e3416b796219025d8a1400a6b25b30dd43c04
Matthias Wagener <m.wagener@netconomy.net>
2018-01-20 22:16:29 +0100

    some first chat functionality

---
+++ server/utils/message.test.js
@@ -0,0 +1,9 @@
+var expect = require('expect');
+var {generateMessage} = require('./message');
+
+describe ('generate message', () =>{
+ it('should generate the correct message object', () => {
+  //store response in variable
+  
+ });
+});
